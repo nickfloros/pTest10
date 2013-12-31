c21ff864d3e753696b76a241b748c88af41d7c47
nickfloros <nickfloros@gmail.com>
2013-12-31 00:35:30 +0000

    retrive anemometer readings

--- web/linechart.dart
+++ web/linechart.dart
@@ -1,4 +1,9 @@
 import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:async';
+import 'dart:js' show context, JsObject, JsFunction;
+
+import 'package:pTest10/chart_elements.dart';
 
 /**
  * A Polymer click counter element.
@@ -7,20 +12,76 @@ import 'package:polymer/polymer.dart';
 class LineChart extends PolymerElement {
   
   bool get applyAuthorStyles => true;
-  
+
+  /*
+   * chart title
+   */
+  @published String chartTitle;
+  /**
+   * x axis title
+   */
   @published String xaxisTitle;
+  /*
+   * y axis title
+   */
   @published String yaxisTitle;
+  /*
+   * min y value
+   */
+  @published String minValue;
+  /*
+   * max y value; settable from 
+   */
+  @published String maxValue;
+  
+  InfoA _infoa;
+  var _gvis;
+  var _chartDiv;
+  var _chartOptions;
+  var _chartData;
+  var _lineChart;
   
   LineChart.created() : super.created() {
-    print('LineChart.created ${id} ${this.getShadowRoot('line-chart')!=null}');
-  }
-
-  void ready() {
-    super.ready();
-    print('LineChart.ready ${id} ${this.getShadowRoot('line-chart')!=null}'); 
+    
+    if (shadowRoot!=null) {
+      _load().then((_) {_intChart();});
+    }
   }
   
-  void fromParent() {
-    print('LineChart.fromParent ${id}');
+  Future _load() {
+    Completer c = new Completer();
+    context["google"].callMethod('load',
+       ['visualization', '1', new JsObject.jsify({
+         'packages': ['corechart'],
+         'callback': new JsFunction.withThis(c.complete)
+       })]);
+    return c.future;
   }
+
+  void _intChart() {
+    
+    var options={
+      'title': chartTitle,
+      'hAxis': {'title': xaxisTitle},
+      'vAxis': {'title': yaxisTitle}
+    };
+    // where we will display the chart
+    _chartDiv = shadowRoot.querySelector('#chart');
+    // google visualization library ref
+    _gvis = context["google"]["visualization"];
+    // set chart options ..
+    _chartOptions = new JsObject.jsify(options);
+    // and finally line chart object
+    _lineChart = new JsObject(_gvis["LineChart"], [_chartDiv]);
+    
+  }
+
+  /**
+   * draws a line chart. The data need to be in the following format {[x, y1, y2, y3]}
+   */
+  void draw(List data){
+    _chartData = _gvis.callMethod('arrayToDataTable', [new JsObject.jsify(data)]);
+    _lineChart.callMethod('draw', [_chartData, _chartOptions]);
+  }
+  
 }
--- web/weatherapp.dart
+++ web/weatherapp.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
 import 'package:polymer/polymer.dart';
 import 'linechart.dart';
+import 'package:pTest10/navtabs.dart';
+import 'package:pTest10/mfordgae.dart';
 
 /**
  * A Polymer click counter element.
@@ -8,24 +11,66 @@ import 'linechart.dart';
 class WeatherApp extends PolymerElement {
   
   bool get applyAuthorStyles => true;
+  
+  NavTabs _navTab;
   LineChart _windSpeedLineChart;
   LineChart _windDirectionLineChart;
+
+  Mford_Gae_Services _svc;
   
   WeatherApp.created() : super.created() {
     print('WeatherApp.created ${id} ${shadowRoot!=null}');
-    if (shadowRoot!=null) {
-      _windSpeedLineChart =shadowRoot.querySelector('#lineA');
-      _windDirectionLineChart =shadowRoot.querySelector('#lineB');
+
+    if (shadowRoot!=null) { // there is a strange behaviour if element i
+
+      // bind to navTabs component
+      _navTab = shadowRoot.querySelector('#navTab');
+      window.on[_navTab.selectionEventName].listen(_showSite);
+      
+      _windSpeedLineChart =shadowRoot.querySelector('#speedChart');
+      _windDirectionLineChart =shadowRoot.querySelector('#directionChart');
+      
+      _svc=new Mford_Gae_Services()
+           ..readSites().then( (resp)=>_renderSites(resp));
+      
     }
   }
 
-  void ready() {
-    super.ready();
-    print('WeatherApp.ready ${id} ${shadowRoot!=null}');
-    _windSpeedLineChart =shadowRoot.querySelector('#lineA');
-    _windDirectionLineChart =shadowRoot.querySelector('#lineB');
-    _windDirectionLineChart.fromParent();
-    _windSpeedLineChart.fromParent();
+  void _renderSites(List<Site> sites){
+    _navTab.options.clear();
+    for (var item in sites) {
+      _navTab.options.add(item.stationName);
+    }
+  }
+  
+  void _showSite(CustomEvent data) {
+    _svc.readSite(data.detail).then( _processResponse);
+  }
+  
+  void _processResponse(var resp) {
+    // set headers ... 
+    List directionData = new List() 
+      ..add( new List() 
+      ..add('Time')
+      ..add('Min')
+      ..add('Avg')
+      ..add('Max')
+      );
+    
+    List speedData = new List() 
+      ..add( new List() 
+      ..add('Time')
+      ..add('Min')
+      ..add('Avg')
+      ..add('Max')
+      );            
+      
+    for (AnemometerReading item in resp) {
+      directionData.add(item.direction.toList(item.timeStamp));
+      speedData.add(item.speed.toList(item.timeStamp));
+    }
+    _windSpeedLineChart.draw(speedData);
+    _windDirectionLineChart.draw(directionData);
   }
 }
 
